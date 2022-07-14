b33ec824369c3aeab0c5e84b4c373961ecb26638
devriasatali2020 <nazimhssn1temp@yahoo.com>
2022-07-14 22:22:09 +0500

    second commit

--- app/src/main/java/com/pratikbutani/pdf/ui/MainActivity.java
+++ app/src/main/java/com/pratikbutani/pdf/ui/MainActivity.java
@@ -1,5 +1,6 @@
 package com.pratikbutani.pdf.ui;
 
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
@@ -12,6 +13,8 @@ import android.os.Handler;
 import android.print.PrintAttributes;
 import android.print.PrintDocumentAdapter;
 import android.print.PrintManager;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -42,6 +45,7 @@ import com.itextpdf.layout.property.TextAlignment;
 import com.pratikbutani.pdf.FileUtils;
 import com.pratikbutani.pdf.PdfDocAdapter;
 import com.pratikbutani.pdf.R;
+import com.pratikbutani.pdf.databinding.ActivityMainBinding;
 import com.pratikbutani.pdf.permission.PermissionsActivity;
 import com.pratikbutani.pdf.permission.PermissionsChecker;
 
@@ -68,50 +72,189 @@ import javax.mail.internet.MimeMessage;
 public class MainActivity extends AppCompatActivity {
 
     Context mContext;
-
     PermissionsChecker checker;
-
     String dest;
     Button sentEmailBtn;
+    ActivityMainBinding binding;
+    double RevolutionOnWatch, PITCHOFPROPELLER, Enginemiles, Shipmiles = 0.1, slip;
+    double Passingtime, EngineSpeed, ShipSpeed, MERPM;
+    String inputROW = "", inputpitch = "", inputshipmiles = "", inputtime = ""; // collect input field in string first
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        // call initView method here
+        initView();
+
 
         mContext = getApplicationContext();
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        sentEmailBtn = findViewById(R.id.sendEmail);
-
-
-        setSupportActionBar(toolbar);
-
         checker = new PermissionsChecker(this);
 
 
-        Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
+        dest = getPath();
 
+
+        binding.buttonCreatePdf.setOnClickListener(view -> {
+            createPDF();
+        });
+
+    }
+
+    private String getPath() {
+        Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
         int currentYear = calendar.get(Calendar.YEAR);
         int currentMonth = calendar.get(Calendar.MONTH) + 1;
         int currentDay = calendar.get(Calendar.DAY_OF_MONTH);
+        return FileUtils.getAppPath(mContext) + currentYear + currentMonth + currentDay + "report.pdf";
+    }
 
-//        Toast.makeText(this, "Today's Date: " + currentYear + currentMonth + currentDay, Toast.LENGTH_SHORT).show();
-        dest = FileUtils.getAppPath(mContext) + currentYear + currentMonth + currentDay + "report.pdf";
+    private void openEmail() {
+        Intent intent = new Intent(MainActivity.this, EmailActivity.class);
+        intent.putExtra("filePath", dest);
+        startActivity(intent);
+    }
 
-        sentEmailBtn.setOnClickListener(new View.OnClickListener() {
+
+    // initialize view here
+    private void initView() {
+        // add text change listener to listen any input text
+        binding.inputROW.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, EmailActivity.class);
-                intent.putExtra("filePath", dest);
-                startActivity(intent);
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                inputROW = charSequence.toString();  // collect input when any text change
+                verifyInput();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
             }
         });
+
+
+        // add text change list for density
+        binding.inputpitch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                inputpitch = charSequence.toString();  // collect input when any text change
+                verifyInput();
+            }
+
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        // add text change listener for temperature
+        binding.inputshipmiles.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                inputshipmiles = charSequence.toString();  // collect input when any text change
+                verifyInput();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        binding.inputtime.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                inputtime = charSequence.toString();  // collect input when any text change
+                verifyInput();
+            }
+
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+
+    }
+
+    // always call this method when there will be any input change
+    @SuppressLint("SetTextI18n")
+    private void verifyInput() {
+        // check if user has entered the input then calculate result
+        if (!inputROW.isEmpty() && !inputpitch.isEmpty() && !inputshipmiles.isEmpty()) {
+            // everything is ready to calculat
+            // convert all string value into double
+            RevolutionOnWatch = Double.parseDouble(inputROW);
+            PITCHOFPROPELLER = Double.parseDouble(inputpitch);
+            Shipmiles = Double.parseDouble(inputshipmiles);
+
+            Enginemiles = RevolutionOnWatch * PITCHOFPROPELLER / 1852;
+            slip = ((Enginemiles - Shipmiles) / Enginemiles) * 100;
+
+            // if doesn;t write Passingtime passing time consider as 24
+            Passingtime = 24;
+            // if write Passingtime then read
+            if (!inputtime.isEmpty()) {
+                Passingtime = Double.parseDouble(inputtime);
+            }
+
+            EngineSpeed = Enginemiles / Passingtime;
+            ShipSpeed = Shipmiles / Passingtime;
+            MERPM = RevolutionOnWatch / (Passingtime * 60);
+
+
+            Enginemiles = Math.round(Enginemiles * 10); //rounding the numbers
+            Enginemiles = Enginemiles / 10;
+
+            slip = Math.round(slip * 100);//rounding the numbers
+            slip = slip / 100;
+
+            EngineSpeed = Math.round(EngineSpeed * 100);//rounding the numbers
+            EngineSpeed = EngineSpeed / 100;
+
+            ShipSpeed = Math.round(ShipSpeed * 100);//rounding the numbers
+            ShipSpeed = ShipSpeed / 100;
+
+            MERPM = Math.round(MERPM * 10);//rounding the numbers
+            MERPM = MERPM / 10;
+
+
+            binding.txtResult1.setText(Double.toString(Enginemiles));
+            binding.txtResult2.setText(Double.toString(slip));
+
+            binding.txtResult3.setText(Double.toString(EngineSpeed));
+            binding.txtResult4.setText(Double.toString(ShipSpeed));
+            binding.txtResult5.setText(Double.toString(MERPM));
+
+        }
+
     }
 
     public void createPdf(String dest) {
-
         if (new File(dest).exists()) {
             new File(dest).delete();
         }
@@ -132,7 +275,7 @@ public class MainActivity extends AppCompatActivity {
 
             Document document = new Document(pdfDocument, PageSize.A4, true);
 
-            /***
+            /*
              * Variables for further use....
              */
             Color mColorAccent = new DeviceRgb(153, 204, 255);
@@ -142,7 +285,7 @@ public class MainActivity extends AppCompatActivity {
             float mValueFontSize = 20.0f;
 
 
-            /**
+            /*
              * How to USE FONT....
              */
             PdfFont font = PdfFontFactory.createFont("assets/fonts/brandon_medium.otf", "UTF-8", true);
@@ -156,39 +299,39 @@ public class MainActivity extends AppCompatActivity {
             // Adding Chunks for Title and value
             addNewIte(document, TextAlignment.LEFT, "Pitch of Propeller:", mHeadingFontSize, font, mColorAccent);
             // Pitch of Propeller detail
-            addNewIte(document, TextAlignment.CENTER, "123123", mValueFontSize, font, mColorBlack);
+            addNewIte(document, TextAlignment.CENTER, inputpitch, mValueFontSize, font, mColorBlack);
             // add space line
             addSpace(document);
 
-
-            // M/E Revolutions on watch field
-            addNewIte(document, TextAlignment.LEFT, "M/E Revolutions on watch:", mHeadingFontSize, font, mColorAccent);
-            // Pitch of Propeller detail
-            addNewIte(document, TextAlignment.CENTER, "06/07/2017", mValueFontSize, font, mColorBlack);
-            // add space line
-            addSpace(document);
 
 
             // Engine Miles [nautical miles] field
             addNewIte(document, TextAlignment.LEFT, "M/E Revolutions on watch:", mHeadingFontSize, font, mColorAccent);
             // Pitch of Propeller detail
-            addNewIte(document, TextAlignment.CENTER, "0.0", 40.f, font, mColorBlack);
+            addNewIte(document, TextAlignment.CENTER, inputROW, 40.f, font, mColorBlack);
             // add space line
             addSpace(document);
 
 
             // ship Miles [nautical miles] field
-            addNewIte(document, TextAlignment.LEFT, "ship Miles [nautical miles]:", mHeadingFontSize, font, mColorAccent);
+            addNewIte(document, TextAlignment.LEFT, "Engine Miles [nautical miles]:", mHeadingFontSize, font, mColorAccent);
             // Pitch of Propeller detail
-            addNewIte(document, TextAlignment.CENTER, "8932", mValueFontSize, font, mColorBlack);
+            addNewIte(document, TextAlignment.CENTER, String.valueOf(Enginemiles), mValueFontSize, font, mColorBlack);
+            // add space line
+            addSpace(document);
+
+            // ship Miles [nautical miles] field
+            addNewIte(document, TextAlignment.LEFT, "Ship Miles [nautical miles]:", mHeadingFontSize, font, mColorAccent);
+            // Pitch of Propeller detail
+            addNewIte(document, TextAlignment.CENTER, inputshipmiles, 30.f, font, mColorBlack);
             // add space line
             addSpace(document);
 
 
             // ship [ ] field
-            addNewIte(document, TextAlignment.LEFT, "ship [ ]:", mHeadingFontSize, font, mColorAccent);
+            addNewIte(document, TextAlignment.LEFT, "ship [%]:", mHeadingFontSize, font, mColorAccent);
             // Pitch of Propeller detail
-            addNewIte(document, TextAlignment.CENTER, "0.00", 40.f, font, mColorBlack);
+            addNewIte(document, TextAlignment.CENTER, String.valueOf(slip), 30.f, font, mColorBlack);
             // add space line
             addSpace(document);
 
@@ -196,7 +339,7 @@ public class MainActivity extends AppCompatActivity {
             // ship [ ] field
             addNewIte(document, TextAlignment.LEFT, "Passing time hours:", mHeadingFontSize, font, mColorAccent);
             // Pitch of Propeller detail
-            addNewIte(document, TextAlignment.CENTER, "10:30:50/10/10/2022", mValueFontSize, font, mColorBlack);
+            addNewIte(document, TextAlignment.CENTER, inputtime, mValueFontSize, font, mColorBlack);
 
             // add space line
             addSpace(document);
@@ -205,7 +348,7 @@ public class MainActivity extends AppCompatActivity {
             // ship [ ] field
             addNewIte(document, TextAlignment.LEFT, "Engine speed [knotes]:", mHeadingFontSize, font, mColorAccent);
             // Pitch of Propeller detail
-            addNewIte(document, TextAlignment.CENTER, "0.00", 30.f, font, mColorBlack);
+            addNewIte(document, TextAlignment.CENTER, String.valueOf(EngineSpeed), 30.f, font, mColorBlack);
 
             // add space line
             addSpace(document);
@@ -214,7 +357,7 @@ public class MainActivity extends AppCompatActivity {
             // ship [ ] field
             addNewIte(document, TextAlignment.LEFT, "Ship,s Speed [knotes]:", mHeadingFontSize, font, mColorAccent);
             // Pitch of Propeller detail
-            addNewIte(document, TextAlignment.CENTER, "0.00", 30.f, font, mColorBlack);
+            addNewIte(document, TextAlignment.CENTER, String.valueOf(ShipSpeed), 30.f, font, mColorBlack);
 
             // add space line
             addSpace(document);
@@ -223,7 +366,7 @@ public class MainActivity extends AppCompatActivity {
             // ship [ ] field
             addNewIte(document, TextAlignment.LEFT, "M/V [rpm]:", mHeadingFontSize, font, mColorAccent);
             // Pitch of Propeller detail
-            addNewIte(document, TextAlignment.CENTER, "0.00", 30.f, font, mColorBlack);
+            addNewIte(document, TextAlignment.CENTER, String.valueOf(MERPM), 30.f, font, mColorBlack);
 
             // add space line
             addSpace(document);
@@ -232,7 +375,7 @@ public class MainActivity extends AppCompatActivity {
 
             Toast.makeText(mContext, "Save to " + dest, Toast.LENGTH_SHORT).show();
 
-
+            PrintPDF(dest);
         } catch (IOException e) {
             LOGE("createPdf: Error " + e.getLocalizedMessage());
         } catch (ActivityNotFoundException ae) {
@@ -240,7 +383,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void PrintPDDF(String path) {
+    private void PrintPDF(String path) {
         try {
             if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
                 PrintManager printManager = (PrintManager) getSystemService(Context.PRINT_SERVICE);
@@ -257,56 +400,13 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == PermissionsActivity.PERMISSIONS_GRANTED) {
-            Toast.makeText(mContext, "Permission Granted to Save", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(mContext, "Permission not granted, Try again!", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void createPDF(View view) {
+    private void createPDF() {
         if (checker.lacksPermissions(REQUIRED_PERMISSION)) {
             PermissionsActivity.startActivityForResult(MainActivity.this, PERMISSION_REQUEST_CODE, REQUIRED_PERMISSION);
         } else {
             createPdf(dest);
         }
     }
-
-    public void printPdf(View view) {
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                PrintPDDF(dest);
-
-            }
-        }, 1000);
-    }
-
 
     private void addNewItemLeftRight(Document document, String leftItem, String rightItem) {
 
@@ -327,7 +427,7 @@ public class MainActivity extends AppCompatActivity {
         document.add(mOrderDetailsTitleParagraph);
     }
 
-
+    // adding space line between order details views
     void addSpace(Document document) {
         // LINE SEPARATOR
         LineSeparator lineSeparator = new LineSeparator(new DottedLine());
@@ -338,6 +438,17 @@ public class MainActivity extends AppCompatActivity {
         document.add(lineSeparator);
         // Adding Line Breakable Space....
         document.add(new Paragraph(""));
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == PermissionsActivity.PERMISSIONS_GRANTED) {
+            Toast.makeText(mContext, "Permission Granted to Save", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(mContext, "Permission not granted, Try again!", Toast.LENGTH_SHORT).show();
+        }
     }
 
 
